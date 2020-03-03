bc8edbc79182d7b69d827a0be04ce72a4dd4e020
Mauricio Cano <mcano.programas@gmail.com>
2020-03-03 15:42:08 +0100

    fixing mkdir issues

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ try {
 function installPMD(){
   var download = 'wget https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.19.0/pmd-bin-6.19.0.zip -P /tmp'
   var unzip = 'unzip /tmp/pmd-bin-6.19.0.zip -d /tmp'
-  var mk = 'mkdir $HOME/pmd'
+  var mk = 'mkdir -p $HOME/pmd'
   var mv = 'mv /tmp/pmd-bin-6.19.0/* $HOME/pmd'
   exec(download+' && '+unzip+' && '+mk+' && '+mv, function(error, stdout, stderr){
     if(error) core.setFailed(stderr)
